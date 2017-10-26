c0ec4a0a9ebe0b7c7bcea3cc05741dd6503644e4
Julian Monticelli <JMM337@pitt.edu>
2017-10-26 11:43:32 -0400

    Added test skeletons

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import sweets.Card;
+
+public class CardTest {
+
+    // Incase anybody hasn't taken 1632, here's how you write a test
+    // Also, if you haven't taken 1632, I HIGHLY suggest you do before you graduate.
+
+    /*
+    @Test
+    public void testThisOrThat() {
+        value = "lol hey";
+        value2 = "soup bruh";
+        assertEquals(value, value2); // This test will fail.
+    }
+
+
+    */
+    
+    
+}
+

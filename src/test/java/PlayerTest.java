ff6e922fba12acd051f50488e0f89f9adb1d2a40
Nathan <nd.bova@gmail.com>
2017-12-06 11:15:25 -0500

    Tests for dad mode

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -18,10 +18,10 @@ public class PlayerTest {
 
 
     */
-    
-	/* 
+
+	/*
 	*  Tests to make sure that if a player has boomerangs, throwBoomerang() will decrement the # of boomerangs they have by 1
-	*/ 
+	*/
 	@Test
 	public void throwBoomerangTestWithBoomerang() {
 		Player testPlayer = new Player(null, "test player 1", 0, 2, 1);	// Player created with 2 boomerangs
@@ -31,19 +31,37 @@ public class PlayerTest {
 
 		assertTrue(countBefore - countAfter == 1);
 	}
-	
-	/* 
+
+	/*
 	*  Tests to make sure that if a player has no boomerangs, throwBoomerang() will not decrement the # of boomerangs
-	*/ 
+	*/
 	@Test
 	public void throwBoomerangTestNoBoomerang() {
 		Player testPlayer = new Player(null, "test player 1", 0, 0, 1);	// Player created with 0 boomerangs
 		int countBefore = testPlayer.getBoomerangCount();
 		testPlayer.throwBoomerang();
 		int countAfter = testPlayer.getBoomerangCount();
-		
+
 		assertTrue(countBefore == countAfter);
 	}
-    
-}
 
+  @Test
+  public void dadIsDad() {
+    Player dad = new Player(null, "Dad", 0, 0, 0);
+    assertTrue(dad.isDad());
+  }
+
+  @Test
+  public void otherPeopleAreNotDad() {
+    Player mom = new Player(null, "Mom", 0, 0, 0);
+    assertFalse(mom.isDad());
+    Player dadLowercase = new Player(null, "dad", 0, 0, 0);
+    assertFalse(dadLowercase.isDad());
+    Player daddy = new Player(null, "Daddy", 0, 0, 0);
+    assertFalse(daddy.isDad());
+    Player dada = new Player(null, "Dada", 0, 0, 0);
+    assertFalse(dada.isDad());
+    Player da = new Player(null, "Da", 0, 0, 0);
+    assertFalse(da.isDad());
+  }
+}

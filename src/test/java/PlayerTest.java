c4f81285efb135b762f633a629f8393f7443a11f
ewright <ecwright4@gmail.com>
2017-12-06 00:48:40 -0500

    Added and fixed unit tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -19,6 +19,31 @@ public class PlayerTest {
 
     */
     
+	/* 
+	*  Tests to make sure that if a player has boomerangs, throwBoomerang() will decrement the # of boomerangs they have by 1
+	*/ 
+	@Test
+	public void throwBoomerangTestWithBoomerang() {
+		Player testPlayer = new Player(null, "test player 1", 0, 2, 1);	// Player created with 2 boomerangs
+		int countBefore = testPlayer.getBoomerangCount();
+		testPlayer.throwBoomerang();
+		int countAfter = testPlayer.getBoomerangCount();
+
+		assertTrue(countBefore - countAfter == 1);
+	}
+	
+	/* 
+	*  Tests to make sure that if a player has no boomerangs, throwBoomerang() will not decrement the # of boomerangs
+	*/ 
+	@Test
+	public void throwBoomerangTestNoBoomerang() {
+		Player testPlayer = new Player(null, "test player 1", 0, 0, 1);	// Player created with 0 boomerangs
+		int countBefore = testPlayer.getBoomerangCount();
+		testPlayer.throwBoomerang();
+		int countAfter = testPlayer.getBoomerangCount();
+		
+		assertTrue(countBefore == countAfter);
+	}
     
 }
 
